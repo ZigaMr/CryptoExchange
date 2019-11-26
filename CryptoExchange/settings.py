5202ccae1c40468f107f0e49ca1673a4b29d0af7
Leviathan1993 <ziga.mrzljak93@gmail.com>
2019-11-26 02:09:33 +0100

    Copied index template

--- CryptoExchange/settings.py
+++ CryptoExchange/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'CryptoExchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\Ziga\Desktop\Projekti\DjangoProjects\CryptoExchange\tradeview\templates\tradeview'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,7 +113,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), # your static/ files folder
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
