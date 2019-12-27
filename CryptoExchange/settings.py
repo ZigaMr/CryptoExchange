2a6c7bfd0eeb49f37037e9cb9b4f49241afb8f70
zigamr <ziga.mrzljak@gmail.com>
2019-12-27 18:37:17 +0100

    Fixed orderbook fill, signup/login form

--- CryptoExchange/settings.py
+++ CryptoExchange/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tradeview.apps.TradeViewConfig',
-    # 'tradeview',
+    # 'tradeview.apps.TradeViewConfig',
+    'tradeview',
     # 'background_task',
 ]
 
